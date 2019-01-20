004881f6c870e05fb7025c20914050b3c4e40193
zhaowei <jameszow@outlook.com>
2019-01-20 16:09:02 +0800

    修改为fallback

--- springcloud-jameszow-feign/src/main/java/com/jameszow/feign/service/FeignExecueCommonService.java
+++ springcloud-jameszow-feign/src/main/java/com/jameszow/feign/service/FeignExecueCommonService.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
  * 描述:
  * Feign Service层
  * 此接口是掉用sping-cloud-common服务
- *
+ * 添加了对Feign配置的Hystrix断路器的支持,Dalston版本应该 添加一个properties文件 并且声明
+ * feign.hystrix.enabled=true 然后开启断路器
  * @author James Zow
  * @create 2019-01-19 18:38
  */
-@FeignClient(value = "spring-cloud-common",fallbackFactory = FeignExecueCommonErrorServiceImpl.class)
+@FeignClient(value = "spring-cloud-common",fallback = FeignExecueCommonErrorServiceImpl.class)
 public interface FeignExecueCommonService {
 
     @RequestMapping(value = "/hi")
